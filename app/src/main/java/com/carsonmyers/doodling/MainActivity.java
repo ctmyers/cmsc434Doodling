2eb94781474a9530ea3c60785b1426a3024d1e8e
Carson Myers <Carson Myers>
2016-11-03 21:53:45 -0400

    init

---
+++ app/src/main/java/com/carsonmyers/doodling/MainActivity.java
@@ -0,0 +1,13 @@
+package com.carsonmyers.doodling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
